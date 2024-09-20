3a20bd24ea38e2047ca2930876eb16510642e9b8
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2024-09-20 18:11:57 +0200

    fikser up script til å gå mot lokal versjon

--- up.sh
+++ up.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 docker compose -p dokgen_dev_interactive up -d --build
 
-export HEALTHCHECK_URL=localhost:8080/actuator/health
-export CONTAINER=dokgen_dev_interactive
+export HEALTHCHECK_URL=dokgen:8080/actuator/health
+export NETWORK_NAME=dokgen_dev_interactive_default
 attempt_counter=0
 max_attempts=15
+
+if [[ $(uname -m) == *"arm64"* ]]; then
+    PLATFORM_FLAG="--platform linux/amd64"
+else
+    PLATFORM_FLAG=""
+fi
+
 printf 'Waiting for server         '
-until $(docker run --rm --network container:$CONTAINER appropriate/curl --output /dev/null --silent --head --fail $HEALTHCHECK_URL); do
+until $(docker run --rm $PLATFORM_FLAG --network $NETWORK_NAME appropriate/curl --output /dev/null --silent --head --fail $HEALTHCHECK_URL); do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
       echo "Max attempts reached, failing"
       exit 1
